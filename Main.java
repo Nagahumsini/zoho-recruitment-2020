a33a6a82c4f0432b0ff35e2532e3969c068efafd
Nagahumsini <77156212+Nagahumsini@users.noreply.github.com>
2021-01-11 09:19:33 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) 
+  {
+	 Scanner s=new Scanner(System.in);
+   int a=s.nextInt();
+   for(int i=a;i>0;i--)
+   {
+   for (int j=5;j>i;j--){
+   System.out.print(j+"  ");}
+   for(int j=i;j>0;j--){
+   System.out.print(i+"  ");}
+   System.out.println(" ");
+   }
+ }
+}
